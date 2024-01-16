7c760c305164692e54897adba4e06f1cd71a4356
sojinshin <jane20180@naver.com>
2024-01-16 22:03:42 +0900

    first commit

---
+++ src/screens/MyPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+type DetailsProps = {
+  navigation: StackNavigationProp<any, 'Details'>; // 'Details'는 스택 네비게이터의 화면 이름
+};
+
+const MyPage: React.FC<DetailsProps> = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go back to Home"
+        onPress={() => navigation(-1)}
+      />
+    </View>
+  );
+}
+
+export default MyPage;
